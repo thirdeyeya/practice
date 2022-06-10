b036b920e19c402e4bd55e253ee9496461e6d27b
Sakika Tsutsui <longlongjourney@outlook.jp>
2022-06-10 05:12:14 +0000

    commit

---
+++ app/Profile.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    protected $guarded = array('id');
+
+    // 以下を追記
+    public static $rules = array(
+        'name' => 'required',
+        'gender' => 'required',
+        'hobby' => 'required',
+        'introduction' => 'required',
+    );
+
+    // 以下を追記
+    // News Modelに関連付けを行う
+    public function histories()
+    {
+        return $this->hasMany('App\History');
+
+    }
+}
